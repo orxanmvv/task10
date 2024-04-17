3010f504deca698d66fc82a0410d9bce7f88d4b9
orxanmvv <m.orxan.m123@gmail.com>
2024-04-18 03:00:18 +0400

    10 cu task

---
+++ ConsoleApp23/ConsoleApp23/Hotel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    internal class Hotel
+    {
+        List<Hotel> hotels = new List<Hotel>();
+        public string Name { get; set; }
+        public int Id { get; set; }
+        private int _id;
+        private List<Room> Rooms { get; set; } = new List<Room>();
+        public Hotel()
+        {
+            Id = ++_id;
+        }
+
+        public Hotel(string name)
+        {
+            Name = name;
+        }
+
+        public void AddRoom(Room room)
+        {
+            Rooms.Add(room);
+        }
+        public List<Hotel> Findhotel(int id)
+        {
+            return hotels.FindAll(x => x.Id == id);
+        }
+        public void ShowAllHotel()
+        {
+            Console.WriteLine($"name{Name} id{Id}");
+        }
+
+        public void MakeReservation(int? roomId)
+        {
+            if (roomId == null)
+            {
+                throw new NullReferenceException("bele otaq yoxdur");
+            }
+
+            Rooms.Find(r => r.Id == roomId);
+            Room room = Rooms.Find(r => r.Id == roomId);
+
+
+
+        }
+    }
+}
